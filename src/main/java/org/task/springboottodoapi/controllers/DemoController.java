ccb3090e1d6c08a8be88801f53356ceaa71c923f
KEVIN-117 <krodriguez17@gmail.com>
2025-06-09 23:54:08 -0400

    feat: Set up base configuration

---
+++ src/main/java/org/task/springboottodoapi/controllers/DemoController.java
@@ -0,0 +1,10 @@
+package org.task.springboottodoapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+}
---
+++ src/test/java/org/task/springboottodoapi/SpringBootTodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.task.springboottodoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
